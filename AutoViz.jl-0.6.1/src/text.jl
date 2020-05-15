bad1bddadb46516d19fa730184a248effd4bfbe5
JiayinYang <jynyang@ucdavis.edu>
2020-05-15 03:28:47 -0400

    Add files via upload

---
+++ AutoViz.jl-0.6.1/src/text.jl
@@ -0,0 +1,13 @@
+isrenderable(::Type{String}) = true
+
+function render!(rm::RenderModel, t::String)
+    font_size = 12
+    x = 10
+    y = 1.5*font_size
+    y_jump = 1.5 * font_size
+    for line in split(t, '\n')
+        add_instruction!(rm, render_text, (line, x, y, font_size, colorant"white"), incameraframe=false)
+        y += y_jump
+    end
+    return rm
+end
