8060b87740db23df2a98d070d559dd6c23c43b22
JiayinYang <jynyang@ucdavis.edu>
2020-05-15 03:15:48 -0400

    Add files via upload

---
+++ AutoViz.jl-0.6.1/src/AutoViz.jl
@@ -0,0 +1,49 @@
+__precompile__(true)
+
+module AutoViz
+
+using Reexport
+using Parameters
+using StaticArrays
+using AutomotiveDrivingModels
+
+@reexport using Colors
+@reexport using Cairo
+
+import Reel
+Reel.set_output_type("gif")
+
+export
+        DEFAULT_CANVAS_WIDTH,
+        DEFAULT_CANVAS_HEIGHT,
+        render!,
+        get_pastel_car_colors,
+
+        Renderable,
+        render,
+        isrenderable,
+
+        ArrowCar
+
+
+const DEFAULT_CANVAS_WIDTH = 1000
+const DEFAULT_CANVAS_HEIGHT = 600
+
+include("colorscheme.jl")
+include("rendermodels.jl")
+
+include("cameras.jl")
+
+include("renderable.jl")
+include("arrowcar.jl")
+include("text.jl")
+
+include("interface.jl")
+include("overlays.jl")
+include("reel_drive.jl")
+
+include("1d/main.jl")
+include("2d/main.jl")
+
+
+end # module
